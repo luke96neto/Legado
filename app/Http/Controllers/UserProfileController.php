75f16640328745114ad2d53d550d15f4a1699b07
Ranieri Valença <ranierivalenca@gmail.com>
2025-06-09 16:30:48 -0300

    feat(user profile / GH-9): Adicionando tabela, model, rotas e relacionamentos para UserProfile

---
+++ app/Http/Controllers/UserProfileController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserProfileRequest;
+use App\Http\Requests\UpdateUserProfileRequest;
+use App\Models\UserProfile;
+
+class UserProfileController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store($request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserProfile $userProfile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update($request, UserProfile $userProfile)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -18,7 +19,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
+    Route::resource('/user-profile', UserProfileController::class)
+        ->only(['store', 'show', 'update']);
 });
 
 require __DIR__.'/auth.php';
