fc3869b42e794d1d1571a79b7725eee722f7bd74
davibala <davidsantosdesouza777@gmail.com>
2025-06-19 22:10:41 -0300

    refactor(authentication / GH-1): movido as rotas OAuth para o middleware guest

--- routes/auth.php
+++ routes/auth.php
@@ -9,16 +9,6 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/auth/github/redirect', [AuthenticatedSessionController::class, 'githubRedirect'])
-    ->name('github.login');
-
-Route::get('/auth/github/callback', [AuthenticatedSessionController::class, 'githubCallback']);
-
-Route::get('/auth/google/redirect', [AuthenticatedSessionController::class, 'googleRedirect'])
-    ->name('google.login');
-
-Route::get('/auth/google/callback', [AuthenticatedSessionController::class, 'googleCallback']);
-
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
@@ -29,6 +19,16 @@ Route::middleware('guest')->group(function () {
         ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+    Route::get('/auth/github/redirect', [AuthenticatedSessionController::class, 'githubRedirect'])
+        ->name('github.login');
+
+    Route::get('/auth/github/callback', [AuthenticatedSessionController::class, 'githubCallback']);
+
+    Route::get('/auth/google/redirect', [AuthenticatedSessionController::class, 'googleRedirect'])
+        ->name('google.login');
+
+    Route::get('/auth/google/callback', [AuthenticatedSessionController::class, 'googleCallback']);
 });
 
 Route::middleware('auth')->group(function () {
