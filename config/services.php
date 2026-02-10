8d882a03dbf7f4f732f03d67b7712f0ad84c5db1
david16sds <davidsantosdesouza777@gmail.com>
2026-02-10 17:40:53 -0300

    fix: Desabilitado a verificação SSL em ambiente local

--- config/services.php
+++ config/services.php
@@ -40,11 +40,17 @@ return [
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
         'redirect' => env('GITHUB_REDIRECT_URI'),
         'scopes' => ['user:email', 'repo'],
+        'guzzle' => [
+            'verify' => env('APP_ENV') === 'production',
+        ],
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI'),
+        'guzzle' => [
+            'verify' => env('APP_ENV') === 'production',
+        ],
     ],
 ];
