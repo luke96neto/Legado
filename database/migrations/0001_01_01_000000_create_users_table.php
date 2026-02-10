f0d3619c7184b3e8a4ffc9f1c7177e87f2b7f1ce
david16sds <davidsantosdesouza777@gmail.com>
2026-02-10 17:00:54 -0300

    fix: Corrigido problema de dados muito extensos para a coluna google_token

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('google_id')->nullable();
-            $table->string('google_token')->nullable();
+            $table->longText('google_token')->nullable();
             $table->string('google_refresh_token')->nullable();
             $table->string('github_id')->nullable();
             $table->string('github_token')->nullable();
